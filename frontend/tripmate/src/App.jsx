efccfb1386f636144f3bc096b594f59c26a75410
shekharshrivas <shekharshrivas07@gmail.com>
2025-03-23 10:28:43 +0530

    made some changes in App.jsx

--- frontend/tripmate/src/App.jsx
+++ frontend/tripmate/src/App.jsx
@@ -1,6 +1,6 @@
-// src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Navbar, Footer } from "./components";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Browse from "./pages/Browse";
 import ListItem from "./pages/ListItem";
@@ -8,20 +8,18 @@ import Login from "./pages/Login";
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/browse" element={<Browse />} />
-            <Route path="/list-item" element={<ListItem />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/browse" element={<Browse />} />
+          <Route path="/list-item" element={<ListItem />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
